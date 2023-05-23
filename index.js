832519472b9db669d698610d6b674b20cf394b9a
tuocan <tooc4n@gmail.com>
2023-05-22 22:17:58 -0400

    upload site files

---
+++ index.js
@@ -0,0 +1,25 @@
+let finalDate = new Date(1685073600*1000)
+let countdown = {
+    seconds: document.getElementById('seconds'),
+    minutes: document.getElementById('minutes'),
+    hours: document.getElementById('hours'),
+    days: document.getElementById('days'),
+};
+function countDown() {
+    let date = new Date();
+    month = date.getMonth()+1;
+    if (month==5) {
+        dateDifference=(finalDate-date);
+        dateDifferenceSeconds = Math.floor((dateDifference / 1000) % 60);
+        dateDifferenceMinutes = Math.floor((dateDifference / 1000 / 60) % 60);
+        dateDifferenceHours = Math.floor((dateDifference / (1000 * 60 * 60)) % 24);
+        dateDifferenceDays = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
+        countdown.days.textContent=dateDifferenceDays + " days.";
+        countdown.hours.textContent=dateDifferenceHours + " hours.";
+        countdown.minutes.textContent=dateDifferenceMinutes + " minutes.";
+        countdown.seconds.textContent=dateDifferenceSeconds + " seconds.";
+    }
+    setTimeout(countDown, 1000);
+}
+
+countDown()
