3b2ce0fbe0321dcaa2801a4f962a63bf05f5fd92
Tou Cann <123590545+tuocan@users.noreply.github.com>
2023-05-26 00:36:32 +0000

    Update index.js
    add check if timer is finished

--- index.js
+++ index.js
@@ -9,20 +9,25 @@ function countDown() {
     let date = new Date();
     month = date.getMonth()+1;
     if (month==5) {
-        dateDifference=(finalDate-date);
-        dateDifferenceSeconds = Math.floor((dateDifference / 1000) % 60);
-        dateDifferenceMinutes = Math.floor((dateDifference / 1000 / 60) % 60);
-        dateDifferenceHours = Math.floor((dateDifference / (1000 * 60 * 60)) % 24);
-//        dateDifferenceDays = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
-//        if (dateDifferenceDays == 1) {
-//            countdown.days.textContent=dateDifferenceDays + " day.";
-//        }
-//        else if (dateDifferenceDays != 1) {
-//            countdown.days.textContent=dateDifferenceDays + " days.";
-//        }
-        countdown.hours.textContent=dateDifferenceHours + " hours.";
-        countdown.minutes.textContent=dateDifferenceMinutes + " minutes.";
-        countdown.seconds.textContent=dateDifferenceSeconds + " seconds.";
+        if (date < finalDate) {
+            dateDifference=(finalDate-date);
+            dateDifferenceSeconds = Math.floor((dateDifference / 1000) % 60);
+            dateDifferenceMinutes = Math.floor((dateDifference / 1000 / 60) % 60);
+            dateDifferenceHours = Math.floor((dateDifference / (1000 * 60 * 60)) % 24);
+//          dateDifferenceDays = Math.floor(dateDifference / (1000 * 60 * 60 * 24));
+//          if (dateDifferenceDays == 1) {
+//              countdown.days.textContent=dateDifferenceDays + " day.";
+//          }
+//          else if (dateDifferenceDays != 1) {
+//              countdown.days.textContent=dateDifferenceDays + " days.";
+//          }
+            countdown.hours.textContent=dateDifferenceHours + " hours.";
+            countdown.minutes.textContent=dateDifferenceMinutes + " minutes.";
+            countdown.seconds.textContent=dateDifferenceSeconds + " seconds.";
+        }
+        else if(date > finalDate) {
+            countdown.hours.textContent="It's Time."
+        }
     }
     setTimeout(countDown, 1000);
 }
